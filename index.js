d2cba1529a6f6ada65d0dc59814c7e428b1f15e7
Manuel Corales <manuel.corales@trocafone.com>
2021-05-28 21:17:52 -0300

    vamos por el cert

--- index.js
+++ index.js
@@ -4,5 +4,15 @@ const app = express();
 app.get('/', function (req, res) {
     res.send(`Holis ${req.query.nombre ? req.query.nombre : 'qué tal?'}`)
 })
- 
+
+var text={"hello.txt":"Hello World!","bye.txt":"Goodbye Cruel World!"};
+app.get('/.wellknown/acme-challenge',function(req,res){
+   res.set({"Content-Disposition":"attachment; filename=\"req.params.name\""});
+   res.send(text[req.params.name]);
+});
+
+// app.get('/.wellknown/acmechallenge', function (req, res) {
+//     res.send(`Holis ${req.query.nombre ? req.query.nombre : 'qué tal?'}`)
+// })
+
 app.listen(3000);
